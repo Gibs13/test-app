6659738528553d30a6b04ff0675eafaf466e923b
Gibs13 <Gsgwlol@gmail.com>
2017-07-07 16:23:05 +0200

    test

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.intent('sayNumber',
   },
   function(request,response) {
     var number = request.slot('number');
-    return {
+    respone = {
              "version": "1.0",
              "response": {
                "directives": [
@@ -66,7 +66,8 @@ app.intent('sayNumber',
                },
              "sessionAttributes": {}
            }
-        	};
+    };
+		response.send();
   }
 );
 
